351b0f97297f50c3f75f9052a92b3d306312750e
Andrew Kelley <superjoe30@gmail.com>
2014-08-06 20:22:49 -0700

    cmake: simpler FindLaxJson.cmake

--- cmake/FindLaxJson.cmake
+++ cmake/FindLaxJson.cmake
@@ -1,20 +1,15 @@
-# - Try to find LaxJson
-# Once done this will define
-#  LAXJSON_FOUND - System has LaxJson
-#  LAXJSON_INCLUDE_DIRS - The LaxJson include directories
-#  LAXJSON_LIBRARIES - The libraries needed to use LaxJson
-#  LAXJSON_DEFINITIONS - Compiler switches required for using LaxJson
+# Copyright (c) 2014 Andrew Kelley
+# This file is MIT licensed.
+# See http://opensource.org/licenses/MIT
+
+# LAXJSON_FOUND
+# LAXJSON_INCLUDE_DIR
+# LAXJSON_LIBRARY
 
 find_path(LAXJSON_INCLUDE_DIR laxjson.h)
-
-find_library(LAXJSON_LIBRARY NAMES laxjson liblaxjson)
-
-set(LAXJSON_LIBRARIES ${LAXJSON_LIBRARY} )
-set(LAXJSON_INCLUDE_DIRS ${LAXJSON_INCLUDE_DIR} )
+find_library(LAXJSON_LIBRARY NAMES laxjson)
 
 include(FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args(laxjson DEFAULT_MSG
-    LAXJSON_LIBRARY LAXJSON_INCLUDE_DIR)
+find_package_handle_standard_args(LAXJSON DEFAULT_MSG LAXJSON_LIBRARY LAXJSON_INCLUDE_DIR)
 
 mark_as_advanced(LAXJSON_INCLUDE_DIR LAXJSON_LIBRARY)
